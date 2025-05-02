418ba6324f8860a13e93ad64f017180484b7e100
Pedro Luiz <pedroluizpb45@outlook.com>
2025-05-02 09:47:52 -0300

    Criando footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FooterComponent } from '@components/footer/footer.component';
 import { HeaderComponent } from '@components/header/header.component';
 import { PokemonService } from '@services/pokemon.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, AsyncPipe],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
